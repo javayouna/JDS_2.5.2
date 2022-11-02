07b6fb5f6bd88d11bb5aa6669cfdd43224b83d1b
javayouna <javayouna@gmail.com>
2022-11-02 15:28:22 +0900

    UserController

---
+++ jds/src/main/java/com/eoe/jds/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.eoe.jds.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
