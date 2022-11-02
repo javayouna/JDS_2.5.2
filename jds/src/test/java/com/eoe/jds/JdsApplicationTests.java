f56d62958a4e6b1594843ed5f6aceac880142ab9
javayouna <106093094+javayouna@users.noreply.github.com>
2022-11-02 11:29:24 +0900

    Add files via upload

---
+++ jds/src/test/java/com/eoe/jds/JdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eoe.jds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
