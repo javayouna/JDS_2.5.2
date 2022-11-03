c6864c323860fe4e74316411f96334ac0c532a0b
javayouna <106093094+javayouna@users.noreply.github.com>
2022-11-03 17:19:35 +0900

    Delete src directory

--- src/main/java/com/eoe/jds/persistent/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eoe.jds.persistent;
-
-import com.eoe.jds.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-}
