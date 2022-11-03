fe5e0e7ac8aaafce676370edc51f0585a8e6d027
leekav49 <116005279+leekav49@users.noreply.github.com>
2022-11-03 12:54:50 +0900

    Add files via upload

--- src/main/java/com/eoe/jds/persistent/QuestionRepository.java
+++ src/main/java/com/eoe/jds/persistent/QuestionRepository.java
@@ -1,6 +1,8 @@
 package com.eoe.jds.persistent;
 
 import com.eoe.jds.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -14,4 +16,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
     //특정 문자열이 포함되어 있는 데이터 조회를 위한 메서드 생성
     List<Question> findBySubjectLike(String subject);
+    //Pageable 객체를 입력으로 받아 Page<Question> 타입 객체를 리턴하는 findAll 메서드를 생성
+    Page<Question> findAll(Pageable pageable);
 }
--- src/test/java/com/eoe/jds/JdsApplicationTests.java
+++ src/test/java/com/eoe/jds/JdsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.eoe.jds;
 
+import com.eoe.jds.Service.QuestionService;
 import com.eoe.jds.entity.Answer;
 import com.eoe.jds.entity.Question;
 import com.eoe.jds.persistent.AnswerRepository;
@@ -25,19 +26,22 @@ class JdsApplicationTests {
 	@Autowired
 	private AnswerRepository answerRepository;
 
+	@Autowired
+	private QuestionService questionService;
+
 	//데이터베이스 삽입 테스트
 	//@Test
 	void testJpa_insert() {
 		Question q1 = Question.builder()
-				.subject("프로젝트 종료 날짜는?")
-				.content("대충 언제쯤 예상 되나요?")
+				.subject("QUESTION TEST1")
+				.content("QUESTION TEST1")
 				.createDate(LocalDateTime.now())
 				.build();
 		this.questionRepository.save(q1); //첫번째 질문 저장
 
 		Question q2 = Question.builder()
-				.subject("DTO와 Entity의 차이를 아시는 분?")
-				.content("어렵네요.")
+				.subject("QUESTION TEST2")
+				.content("QUESTION TEST2")
 				.createDate(LocalDateTime.now())
 				.build();
 		this.questionRepository.save(q2); //두번째 질문 저장
@@ -52,7 +56,7 @@ class JdsApplicationTests {
 		   디비에 저장된 데이터의 사이즈가 (1) all.size() (전체 사이즈) 인지에 대한 테스트*/
 
 		Question q = all.get(0);
-		assertEquals("DTO와 Entity의 차이를 아시는 분?", q.getSubject());
+		assertEquals("QUESTION TEST1", q.getSubject());
 		/* 디비에 저장된 첫번째 데이터의 (0) 제목이
 			"DTO와 Entity의 차이를 아시는 분?" 와 일치하는지에 대한 테스트 */
 	}
@@ -65,7 +69,7 @@ class JdsApplicationTests {
 		if(oq.isPresent()) {
 			/*isPresent() => null인지 아닌지를 확인*/
 			Question q = oq.get();
-			assertEquals("DTO와 Entity의 차이를 아시는 분?", q.getSubject());
+			assertEquals("QUESTION TEST2", q.getSubject());
 		}
 	}
 
@@ -75,7 +79,7 @@ class JdsApplicationTests {
 	 즉, findBy + 엔티티의 속성명(예:findBySubject)과 같은 리포지터리 메서드를 작성하면 해당 속성의 값으로 데이터를 조회 가능*/
 	//@Test
 	void testJpa_select3() {
-		Question q = this.questionRepository.findBySubject("DTO와 Entity의 차이를 아시는 분?");
+		Question q = this.questionRepository.findBySubject("QUESTION TEST2");
 		assertEquals(2, q.getId());
 	}
 
@@ -83,16 +87,16 @@ class JdsApplicationTests {
 	//@Test
 	void testJpa_select4() {
 		Question q = this.questionRepository.findBySubjectAndContent(
-				"DTO와 Entity의 차이를 아시는 분?", "어렵네요.");
+				"QUESTION TEST2", "QUESTION TEST2");
 		assertEquals(2, q.getId());
 	}
 
 	/*특정 단어가 포함된 데이터값 조회를 위한 테스트*/
 	//@Test
 	void testJpa_select5() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("DTO%");
+		List<Question> qList = this.questionRepository.findBySubjectLike("QUESTION%");
 		Question q = qList.get(0);
-		assertEquals("DTO와 Entity의 차이를 아시는 분?", q.getSubject());
+		assertEquals("QUESTION TEST2", q.getSubject());
 	}
 
 	/*데이터 수정 테스트*/
@@ -102,7 +106,7 @@ class JdsApplicationTests {
 		//assertTrue(값) => 값이 true인지 테스트
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
-		q.changeSubject("수정한 내용입니다.");
+		q.changeSubject("QUESTION UPDATE TEST");
 		this.questionRepository.save(q);
 	}
 
@@ -127,7 +131,7 @@ class JdsApplicationTests {
 		/*질문 데이터를 먼저 구해야하기 때문에 id가 2인 질문 데이터를 가져옴*/
 
 		Answer answer = Answer.builder()
-				.content("")
+				.content("ANSWER TEST")
 				.question(question)
 				/*Answer 엔티티의 question 속성에 가져온 질문 데이터를 대입해 답변 데이터 생성*/
 				.createDate(LocalDateTime.now())
@@ -137,7 +141,7 @@ class JdsApplicationTests {
 
 	/*답변 데이터 조회를 위한 테스트*/
 	//@Test
-	void testJpa() {
+	void testJpa_Answer_select () {
 		Optional<Answer> oa = this.answerRepository.findById(1);
 		/*id의 값이 1인 답변을 조회*/
 		assertTrue(oa.isPresent());
@@ -150,7 +154,7 @@ class JdsApplicationTests {
 	@Transactional
 	/*@Transactional => 테스트에서 DB 세션이 끊어져 오류가 나는 것을 막기 위한 애너테이션*/
 	//@Test
-	void d() {
+	void testJpa_Answer_select2() {
 		Optional<Question> oq = this.questionRepository.findById(2);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
@@ -158,9 +162,18 @@ class JdsApplicationTests {
 		List<Answer> answerList = q.getAnswerList();
 
 		assertEquals(1, answerList.size());
-		assertEquals("인프런에 김영한님 강의를 한번 보세요.", answerList.get(0).getContent());
+		assertEquals("ANSWER TEST", answerList.get(0).getContent());
 
+	}
 
+	/*페이징 구현을 위한 테스트 데이터 300개 삽입*/
+	//@Test
+	void testJpa_sampleData_insert() {
+		for(int i=1; i<=300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "test";
+			this.questionService.create(subject, content);
+		}
 	}
 
 
